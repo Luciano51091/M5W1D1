9e14c1a716754da0330e6b87037c2a2e63181ca8
Luciano Vaccaro <luciano.vaccaro510@gmail.com>
2026-02-15 17:23:02 +0100

    first commit

---
+++ react.js
@@ -0,0 +1,45 @@
+/* 1. Cosa è ReactJs?
+
+ - Una libreria JavaScript per la creazione di interfacce utente */
+
+
+// 2. React è un framework, non una libreria.
+// - Falso
+
+/* 3. Il file package.json contiene...
+
+ - molte informazioni utili, come ad esempio l'elenco di tutte le dipendenze richieste dall'applicazione  
+ 
+ */
+
+// 4. create-react-app è l'unico modo possibile per creare applicazioni React.
+// - Falso
+
+/* 5. Qual è il comando da lanciare nel terminale per creare una nuova create-react-app con nome test?
+
+- npx create-react-app test
+
+
+*/
+
+/* 6. Cosa è un componente React?
+
+- Un blocco logica/contenuto riutilizzabile all'interno dell'applicazione 
+ 
+
+// 7. Un componente React può venire creato in tre modi: come funione, classe o interfaccia 
+// - Falso
+ 
+// 8. Le props sono frammenti di informazione assegnati all'invocazione di un componente React, utili al fine di rendere il componente dinamico e più riutilizzabile 
+// - Vero
+
+// 9. Le props possono essere passate solamente da un componente genitore ad un componente figlio, non è possibile fare il contrario 
+// - Vero
+
+/* 10. Da dove possono venire recuperate le props all'interno di un componente React creato come classe?
+
+- Possono essre recuperate all'interno dell'oggetto "this", dentro un sotto-oggetto chiamato "props"
+
+
+
+
